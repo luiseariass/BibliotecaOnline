88ba46fb7e3c27675b1f46655aa472f6c5de9315
luiseariass <luisearias94@gmail.com>
2020-05-08 20:45:14 -0500

    produccion

--- bibliotecaonline/settings.py
+++ bibliotecaonline/settings.py
@@ -1,4 +1,4 @@
-    'django.contrib.sessions.middleware.SessionMiddleware',
+
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'wkp)es__q5a2wj0*_6m%z6e)t5trur
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
